c75dabd4e9bc1ad3e0ea8c80a26c88554f30748f
kiinosuke <89005440+kiinosuke@users.noreply.github.com>
2021-11-21 10:40:59 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,24 +1,6 @@
-'use strict'
+'use strict';
 
-
-// mainvisual
-const pics_src =["img/adpDSC_4393.jpg",
-"img/forest.jpg","img/adpDSC_4489.jpg"];
-
-let num = -1;
-
-function slideShow_timer () {
-    if (num === 2) {
-        num = 0
-    }else {
-        num++;
-    }
-
-    document.getElementById('mainvisual').src=pics_src[num];
-}
-
-setInterval(slideShow_timer,1000);
-
+// sp-menu
 const menu= document.getElementById('menu');
 const mask = document.getElementById('mask');;
 const modal = document.getElementById('modal');
@@ -38,6 +20,22 @@ close.addEventListener('click',()=>{
     menu.classList.remove('hidden');
     mask.classList.add('hidden');
     modal.classList.add('hidden');
-
 });
 
+// mainvisual
+const pics_src =["img/adpDSC_4393.jpg",
+"img/forest.jpg","img/adpDSC_4489.jpg"];
+
+let num = -1;
+
+function slideShow_timer () {
+    if (num === 2) {
+        num = 0
+    }else {
+        num++;
+    }
+
+    document.getElementById('mainvisual').src=pics_src[num];
+}
+
+setInterval(slideShow_timer,1000);
